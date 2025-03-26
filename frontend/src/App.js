81d2787c2024b7c92bf31a4e510ecd4cf10f484c
JM Batayola <batayols@github.com>
2025-03-26 20:36:49 +0800

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    fetchStudents();
+  }, []);
+
+  const fetchStudents = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/api/students");
+      setStudents(response.data);
+    } catch (error) {
+      console.error("Error fetching students:", error);
+    }
+  };
+
+  const addStudent = async (name, course) => {
+    try {
+      await axios.post("http://127.0.0.1:5000/api/students", { name, course });
+      fetchStudents();
+    } catch (error) {
+      console.error("Error adding student:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Student Recording System</h1>
+      <StudentForm addStudent={addStudent} />
+      <StudentList students={students} />
+    </div>
+  );
+};
+
+export default App;
